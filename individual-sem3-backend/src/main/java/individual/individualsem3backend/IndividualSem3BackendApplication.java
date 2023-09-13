d2ba6e6b07d666d8f700ef94bf6115507af4df3d
ChaiB04 <cbahari66@gmail.com>
2023-09-13 14:37:36 +0200

    New project with springboot oops

---
+++ individual-sem3-backend/src/main/java/individual/individualsem3backend/IndividualSem3BackendApplication.java
@@ -0,0 +1,13 @@
+package individual.individualsem3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualSem3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndividualSem3BackendApplication.class, args);
+    }
+
+}
---
+++ individual-sem3-backend/src/test/java/individual/individualsem3backend/IndividualSem3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package individual.individualsem3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualSem3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
