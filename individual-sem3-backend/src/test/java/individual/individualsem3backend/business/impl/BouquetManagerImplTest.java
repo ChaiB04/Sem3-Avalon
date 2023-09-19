dca74684ebc0f109236bc0af8659a60476f5d47d
ChaiB04 <cbahari66@gmail.com>
2023-09-19 16:09:51 +0200

    Made happy flow cases for BouquetManagerImplTest and a bad flow

--- individual-sem3-backend/src/test/java/individual/individualsem3backend/business/impl/BouquetManagerImplTest.java
+++ individual-sem3-backend/src/test/java/individual/individualsem3backend/business/impl/BouquetManagerImplTest.java
@@ -1,10 +1,6 @@
 package individual.individualsem3backend.business.impl;
 
-import individual.individualsem3backend.business.BouquetManagerUseCase;
-import individual.individualsem3backend.controller.BouquetRequestResponse.CreateBouquetRequest;
-import individual.individualsem3backend.controller.BouquetRequestResponse.CreateBouquetResponse;
-import individual.individualsem3backend.controller.BouquetRequestResponse.GetAllBouquetRequest;
-import individual.individualsem3backend.controller.BouquetRequestResponse.GetAllBouquetResponse;
+import individual.individualsem3backend.controller.BouquetRequestResponse.*;
 import individual.individualsem3backend.domain.Bouquet;
 import individual.individualsem3backend.domain.Flower;
 import individual.individualsem3backend.persistence.BouquetRepository;
@@ -15,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -69,6 +67,8 @@ class BouquetManagerImplTest {
         verify(bouquetRepositoryMock).findAll();
     }
 
+
+
     @Test
     void createProduct() {
         CreateBouquetResponse response = CreateBouquetResponse.builder().build();
@@ -104,18 +104,103 @@ class BouquetManagerImplTest {
 
         assertEquals(expectedResult, actualResult);
 
-
     }
 
     @Test
     void deleteProduct() {
+        Flower flower1 = Flower.builder().id(23).name("Neuvilette")
+                .price(13.32).description("Pretty boy").color("Blue")
+                .lifeExpectancy(32).build();
+
+        Flower flower2 = Flower.builder().id(21).name("Wriothlessly")
+                .price(323.32).description("Handsome boy").color("Black")
+                .lifeExpectancy(10).build();
+
+        Bouquet bouquet1 = Bouquet.builder().id(1).name("Neuviandrizzly").price(123.12)
+                .flowers(List.of(flower1, flower2)).colorOfBow("Blue and black").build();
+
+        bouquetManager.deleteProduct(bouquet1.getId());
+
+        verify(bouquetRepositoryMock).deleteById(bouquet1.getId());
     }
 
     @Test
     void getProduct() {
+        Flower flower1 = Flower.builder().id(23).name("Neuvilette")
+                .price(13.32).description("Pretty boy").color("Blue")
+                .lifeExpectancy(32).build();
+
+        Flower flower2 = Flower.builder().id(21).name("Wriothlessly")
+                .price(323.32).description("Handsome boy").color("Black")
+                .lifeExpectancy(10).build();
+
+        Bouquet bouquet1 = Bouquet.builder().id(1).name("Neuviandrizzly").price(123.12)
+                .flowers(List.of(flower1, flower2)).colorOfBow("Blue and black").build();
+
+        Flower flower3 = Flower.builder().id(2).name("Navia")
+                .price(324.32).description("Pretty girl").color("Yellow")
+                .lifeExpectancy(2).build();
+
+        Flower flower4 = Flower.builder().id(5).name("Chlorande")
+                .price(342.24).description("Handsome girl").color("Purple")
+                .lifeExpectancy(90).build();
+
+        Bouquet bouquet2 = Bouquet.builder().id(3).name("NaviaAndChlorande").price(324.23)
+                .flowers(List.of(flower3, flower4)).colorOfBow("Yellow and purple").build();
+
+        when(bouquetRepositoryMock.findById(any())).thenReturn(bouquet1, bouquet2);
+
+        Optional<Bouquet> actualResult = bouquetManager.getProduct(bouquet1.getId());
+
+        if(actualResult.isEmpty()){
+            actualResult = null;
+        }
+
+        Optional<Bouquet> expectedResult = Optional.ofNullable(bouquet1);
+
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
+    void getNonExistentProduct(){
+        //is this good enough?
+        when(bouquetRepositoryMock.findById(any())).thenReturn(null);
+
+        Optional<Bouquet> actualResult = bouquetManager.getProduct(100); // Assuming 100 is a non-existent ID
+
+        assertFalse(actualResult.isPresent());
+    }
+    @Test
     void updateProduct() {
+
+        Flower flower3 = Flower.builder().id(2).name("Navia")
+                .price(324.32).description("Pretty girl").color("Yellow")
+                .lifeExpectancy(2).build();
+
+        Flower flower4 = Flower.builder().id(5).name("Chlorande")
+                .price(342.24).description("Handsome girl").color("Purple")
+                .lifeExpectancy(90).build();
+
+        Bouquet bouquet2 = Bouquet.builder().id(1).name("NaviaAndChlorande").price(324.23)
+                .flowers(List.of(flower3, flower4)).colorOfBow("Yellow and purple").build();
+
+        when(bouquetRepositoryMock.findById(any())).thenReturn(bouquet2);
+
+        Flower flower1 = Flower.builder().id(23).name("Neuvilette")
+                .price(13.32).description("Pretty boy").color("Blue")
+                .lifeExpectancy(32).build();
+
+        Flower flower2 = Flower.builder().id(21).name("Wriothlessly")
+                .price(323.32).description("Handsome boy").color("Black")
+                .lifeExpectancy(10).build();
+
+
+        UpdateBouquetRequest updateRequest = UpdateBouquetRequest.builder().id(1).name("Neuviandrizzly").price(123.12)
+                .flowers(List.of(flower1, flower2)).colorOfBow("Blue and black").build();
+
+        bouquetManager.updateProduct(updateRequest);
+
+        verify(bouquetRepositoryMock).update(bouquet2);
+
     }
 }
