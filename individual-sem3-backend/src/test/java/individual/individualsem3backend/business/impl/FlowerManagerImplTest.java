93d098aca5e3377abbb2f8a3044b0dea951baf52
ChaiB04 <cbahari66@gmail.com>
2023-09-20 09:41:57 +0200

    commit for branch, Added unit test class for flower manager

---
+++ individual-sem3-backend/src/test/java/individual/individualsem3backend/business/impl/FlowerManagerImplTest.java
@@ -0,0 +1,30 @@
+package individual.individualsem3backend.business.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlowerManagerImplTest {
+
+    @Test
+    void getProducts() {
+
+        //flower test
+    }
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void getProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+}
