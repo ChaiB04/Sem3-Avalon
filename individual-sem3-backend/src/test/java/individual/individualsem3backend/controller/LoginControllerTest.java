0708af4ea3bfc36f19edd0bf7a829fed961a9307
ChaiB04 <cbahari66@gmail.com>
2023-11-30 16:11:51 +0100

    My sonar failed for some reason but it does not fail if I push to sonar manually

--- individual-sem3-backend/src/test/java/individual/individualsem3backend/controller/LoginControllerTest.java
+++ individual-sem3-backend/src/test/java/individual/individualsem3backend/controller/LoginControllerTest.java
@@ -1,16 +1,12 @@
 package individual.individualsem3backend.controller;
 
 import individual.individualsem3backend.business.LoginManager;
-import individual.individualsem3backend.business.exception.UserException;
-import individual.individualsem3backend.persistence.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,22 +28,8 @@ class LoginControllerTest {
     @MockBean
     private LoginManager loginManager;
 
-    @Mock
-    private UserRepository userRepositoryMock;
-
-    @Test
-    void login(){
-
-    }
-
-
     @Test
     void login_withUnregisteredUser_ShouldReturnBadRequest() throws Exception {
-//        String email = "Neuvi@gmail.com";
-//        String password = "Aaaaa";
-//
-//        // Mock the behavior of LoginManager to throw UserException
-//        when(loginManager.userLogin(email, password)).thenThrow(new UserException("Invalid Credentials"));
 
         mockMvc.perform(post("/login")
                         .contentType(APPLICATION_JSON_VALUE)
@@ -56,11 +38,5 @@ class LoginControllerTest {
                 .andExpect(status().isBadRequest());
 
         verifyNoInteractions(loginManager);
-//        verify(loginManager).userLogin(email, password);
     }
-
-
-
-
-
 }
